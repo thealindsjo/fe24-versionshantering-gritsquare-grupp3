2f8be396c93dc453062da8ce5a8e4a523712d903
Kimiya777 <Kimiya.masterifarahani@gmail.com>
2025-03-26 15:12:55 +0100

    fixed postUsers

--- modules/main.js
+++ modules/main.js
@@ -1,6 +1,27 @@
+import {postUser} from './firebase.js';
+
 const toggleMenu = document.querySelector(".toggleMenu");
 const menu = document.querySelector(".menu");
+const messageForm = document.getElementById('addMessage');
 
 toggleMenu.addEventListener("click", () => {
   menu.classList.toggle("active");
+});
+
+
+
+messageForm.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const userName = document.getElementById('user-name').value;
+  const userMessage = document.getElementById('user-message').value;
+
+  console.log(userName, userMessage)
+
+  const userObj = {
+    userName,
+    userMessage,
+  }
+  await postUser(userObj);
+  //await postMessage(Obj);
 });
